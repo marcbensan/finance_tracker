dadaec1edd479ee8ee05b256ad8ba292720c0db1
venomspood <marc.bensan7@gmail.com>
2024-02-01 15:08:26 -0500

    modified api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+require('dotenv').config();
 const cors = require("cors");
+const Transaction = require("./models/transaction");
+const { default: mongoose } = require("mongoose");
 
 app.use(cors());
 app.use(express.json());
@@ -8,8 +11,14 @@ app.get("/api/test", (req, res) => {
   res.json("test ok3");
 });
 
-app.post("/api/transaction", (req, res) => {
-  res.json(req.body);
+app.post("/api/transaction", async (req, res) => {
+  await mongoose.connect(process.env.MONGO_URL)
+  const { name, description, datetime, price } = req.body;
+  const transaction = await Transaction.create({name, description, datetime, price})
+
+  res.json(transaction);
 });
 
 app.listen(4040);
+// wemy9cqCvDjgET2E
+//vdRipY4VW0n3ymzL
--- src/App.js
+++ src/App.js
@@ -11,12 +11,19 @@ function App() {
   function addNewTransaction(e) {
     e.preventDefault();
     const url = process.env.REACT_APP_API_URL + "/transaction";
+    const price = name.split(' ')[0];
     fetch(url, {
       method: "POST",
       headers: { "Content-type": "application/json" },
-      body: JSON.stringify({ name, description, datetime }),
+      body: JSON.stringify({ 
+        name: name.substring(price.length + 1), 
+        description,
+        datetime }),
     }).then((response) => {
       response.json().then(json => {
+        setName("")
+        setDatetime("")
+        setDescription("")
         console.log("result", json);
       });
     });
